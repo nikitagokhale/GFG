192433411b1913447f90bc5344dbd108283547f0
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2022-08-18 13:00:50 +0530

    Update Prog2.java

--- Prog2.java
+++ Prog2.java
@@ -2,12 +2,11 @@ class Solution{
     static int minJumps(int[] arr)
     {
         int noOfJumps = 0, i=0;
-        if(arr.length == 0)
-            return -1;
-        while(i<arr.length)
+        while(i<arr.length-1)
         {
             i += arr[i];
             noOfJumps++;
         }
+        return ((arr.length==0)?-1:noOfJumps);
     }
 }
