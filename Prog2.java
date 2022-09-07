fd168508c6b9f7ca23232739e1dd25c5ec6713c2
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2022-09-07 18:57:50 +0530

    Update Prog2.java

--- Prog2.java
+++ Prog2.java
@@ -1,12 +1,62 @@
+// class Solution{
+//     static int minJumps(int[] arr)
+//     {
+//         int noOfJumps = 0, i=0;
+//         while(i<arr.length-1)
+//         {
+//             i += arr[i];
+//             noOfJumps++;
+//         }
+//         return ((arr.length==0)?-1:noOfJumps);
+//     }
+// }
+
 class Solution{
     static int minJumps(int[] arr)
     {
-        int noOfJumps = 0, i=0;
-        while(i<arr.length-1)
-        {
-            i += arr[i];
-            noOfJumps++;
-        }
-        return ((arr.length==0)?-1:noOfJumps);
+        if (arr.length <= 1) 
+            return 0; 
+  
+        // Return -1 if not possible to jump 
+        if (arr[0] == 0) 
+            return -1; 
+  
+        // initialization 
+        int maxReach = arr[0]; 
+        int step = arr[0]; 
+        int jump = 1; 
+  
+  
+        // Start traversing array 
+        for (int i = 1; i < arr.length; i++) 
+        { 
+            // Check if we have reached the end of the array 
+            if (i == arr.length - 1) 
+                return jump; 
+  
+            // updating maxReach 
+            maxReach = Math.max(maxReach, i+arr[i]); 
+  
+            // we use a step to get to the current index 
+            step--; 
+  
+            // If no further steps left 
+            if (step == 0) 
+            { 
+                //  we must have used a jump 
+                jump++; 
+                   
+                //Check if the current index/position  or lesser index 
+                // is the maximum reach point from the previous indexes 
+                if(i>=maxReach) 
+                   return -1; 
+  
+                // re-initialize the steps to the amount 
+                // of steps to reach maxReach from position i. 
+                step = maxReach - i; 
+            } 
+        } 
+  
+        return -1;
     }
 }
