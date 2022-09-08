64f6b91fb91dc2c0b286bdf12866eb45ba2fe549
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2022-09-08 10:21:22 +0530

    Update Prog5.java

--- Prog5.java
+++ Prog5.java
@@ -1,9 +1,25 @@
 class Tree
 {
+    int height(Node root)
+    {
+        int hl, hr;
+        if(root==NULL)
+            return 0;
+        if(root.left==NULL && root.right==NULL)
+            return 0;
+        hl = height(root.left);
+        hr = height(root.right);
+        if(hl>hr)
+            return hl+1;
+        else
+            return hr+1;
+    }
+    
     //Function to return list containing elements of left view of binary tree.
     ArrayList<Integer> leftView(Node root)
     {
         Node temp = root;
+        int height = height(root);
         ArrayList<Integer> result= new ArrayList<Integer>();
         if(root==null)
             return (result);
